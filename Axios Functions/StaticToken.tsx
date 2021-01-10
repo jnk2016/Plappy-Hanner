c79f49baa2d9e8eb9641debf8071deb26dcd50d2
Jackson Kim <jnk2016@gmail.com>
2021-01-09 19:18:51 -0600

    new branch android

--- Axios Functions/StaticToken.tsx
+++
@@ -1,8 +0,0 @@
-import '@trendmicro/react-sidenav/dist/react-sidenav.css';
-
-export default class XAccessToken{ 
-    static token = '';
-    static getToken(){
-        return this.token;
-    }
-}
